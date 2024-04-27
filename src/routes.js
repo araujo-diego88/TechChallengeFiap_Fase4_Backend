979afa80313394ec9615ef1a1c239b628b51cc4b
Diego Araujo <diego.araujo88.da@hotmail.com>
2024-04-27 14:42:13 -0300

    criamos uma funcionalidade que podemos cadastrar um usuário através de seu e-mail, para um cadastro novo é gerado um ID, caso o usuário possua cadastro a API retornará a sua respectiva ID.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
+import SessionController from "./controllers/SessionController";
+
 const routes = new Router();
 
-routes.get('/', (req, res)=> {
-  return res.json({ ok: true});
-});
+routes.post('/sessions', SessionController.store);
 
 export default routes;
