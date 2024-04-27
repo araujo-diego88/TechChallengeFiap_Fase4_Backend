840e61f0a1407791674ee541075d96f42dfeccba
Diego Araujo <diego.araujo88.da@hotmail.com>
2024-04-27 10:39:02 -0300

    criação do início da estrutura do backend de nossa aplicação, e conexão com o banco de dados MongoDb, algumas bibliotecas também foram instaladas para o auxílio nessa parte do projeto ( express, sucrase, nodemon, mongoose).

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import routes from './routes';
+
+class App{
+
+  constructor(){
+    this.server = express();
+
+    mongoose.connect('mongodb+srv://backFase4:backFase4@backfase4.wpybnh1.mongodb.net/backFase4?retryWrites=true&w=majority&appName=backFase4');
+
+    this.middlewares();
+    this.routes();
+  }  
+
+  middlewares(){
+    this.server.use(express.json());
+  }
+
+  routes(){
+    this.server.use(routes);
+  }
+
+}
+export default new App().server;
